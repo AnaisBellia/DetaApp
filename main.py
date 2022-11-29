c703b341e1b02d1ce8ef44707fd804f7e7640630
anais <anais.bellia@epfedu.fr>
2022-11-29 22:54:19 +0100

    google analytics commit

--- main.py
+++ main.py
@@ -3,5 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
-def hello_world():
-    return "Hello World from Anaïs Bellia"
+
+    def hello_world():
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=UA-250432838-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' YOUR_GA_CODE');
+</script>
+ """
+ return prefix_google + "Hello World"
