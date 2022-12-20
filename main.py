c6c4d6185e315b7f811e861b2d64192687694e26
anais <anais.bellia@epfedu.fr>
2022-12-20 21:18:05 +0100

    last commit

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from flask import Flask, Response, redirect, Request,render_template
-import sys
-import logging
+from flask import Flask, render_template
 import requests
-from oauth2client.service_account import ServiceAccountCredentials
-#from apiclient.discovery import build
-# good
-from googleapiclient.discovery import build
-
-
+import logging
 app = Flask(__name__)
 
 '''
@@ -26,6 +19,8 @@ src="https://www.googletagmanager.com/gtag/js?id=UA-250432838-1"></script>
  """
     return prefix_google + "Hello World from ANAIS BELLIA"
 '''
+
+
 @app.route('/Logger',methods=['GET'])
 def logger():
     page="""
@@ -42,14 +37,13 @@ print('hello world in python console from Anaïs Bellia')
 @app.route('/')
 def home():
     return render_template('base.html')
+
 @app.route('/',methods=['POST'])
 def login():
     text=requests.form['text']
     processedtext=text.upper()
     return processedtext #"""<h1>Welcome</h1> """
 
-
-## cookies
 @app.route('/google',methods=['GET'])
 def get_google_request():
     req = requests.get("https://www.google.com/")
@@ -60,66 +54,6 @@ def get_GA_request():
     req = requests.get("https://analytics.google.com/analytics/web/#/report-home/a250432838w344296954p280819955")
     return req.text #"""<h1>Welcome</h1> """
 
-## jusqu'ici c'est ok
-
-
-#### GET THE NUMBER OF VISITORS
-
-SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
-KEY_FILE_LOCATION = 'myprojectdeta-18043a20e2d1.json'
-VIEW_ID = 'UA-250432838-1' #You can find this in Google Analytics > Admin > Property > View > View Settings (VIEW ID)
-#344309589
-
-def initialize_analyticsreporting():
-  credentials = ServiceAccountCredentials.from_json_keyfile_name(
-      KEY_FILE_LOCATION, SCOPES)
-  analytics = build('analyticsreporting', 'v4', credentials=credentials)
-
-  return analytics
-
-
-def get_report(analytics):
-  return analytics.reports().batchGet(
-      body={
-        'reportRequests': [
-        {
-          'viewId': VIEW_ID,
-          'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
-          'metrics': [{'expression': 'ga:pageviews'}],
-          'dimensions': []
-        }]
-      }
-  ).execute()
-
-
-def get_visitors(response):
-  visitors = 0 # in case there are no analytics available yet
-  for report in response.get('reports', []):
-    columnHeader = report.get('columnHeader', {})
-    metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
-
-    for row in report.get('data', {}).get('rows', []):
-      dateRangeValues = row.get('metrics', [])
-
-      for i, values in enumerate(dateRangeValues):
-        for metricHeader, value in zip(metricHeaders, values.get('values')):
-          visitors = value
-
-  return str(visitors)
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-@app.route('/visitors')
-def visitors():
-  analytics = initialize_analyticsreporting()
-  response = get_report(analytics)
-  visitors = get_visitors(response)
-
-  return render_template('visitors.html', visitors=str(visitors))
-
-##########################
 
 if __name__=='__main__':
     app.run(debug=True)
